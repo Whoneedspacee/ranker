54da96a3e456de0c80b3c6ca86b9821fa7f54254
Space <yougotreallyowned@gmail.com>
2021-12-29 14:40:20 -0500

    Updates

---
+++ precache-manifest.827456c8bd8b7f8ebb76b610f500030b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "745a9a71bbb73c0c681a0aaf3caf4515",
+    "url": "/ranker/index.html"
+  },
+  {
+    "revision": "fa7e73b833a8330753e4",
+    "url": "/ranker/static/css/2.f1f795ee.chunk.css"
+  },
+  {
+    "revision": "9782203565a3ebae4607",
+    "url": "/ranker/static/css/main.62423486.chunk.css"
+  },
+  {
+    "revision": "fa7e73b833a8330753e4",
+    "url": "/ranker/static/js/2.cafdea88.chunk.js"
+  },
+  {
+    "revision": "9efecc6e366a89e935d3722c67df4853",
+    "url": "/ranker/static/js/2.cafdea88.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9782203565a3ebae4607",
+    "url": "/ranker/static/js/main.453044fa.chunk.js"
+  },
+  {
+    "revision": "37e09796ce857403412f",
+    "url": "/ranker/static/js/runtime-main.39825744.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ranker/precache-manifest.36b6886517c84e8c3797aeef4792437c.js"
+  "/ranker/precache-manifest.827456c8bd8b7f8ebb76b610f500030b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ranker/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
